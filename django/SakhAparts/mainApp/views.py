82cfe56ac831d7d8b780cf013705425da371b6cf
astorcode <albertabashin2005@gmail.com>
2024-02-05 06:49:02 +0300

    added index.html as main page

--- django/SakhAparts/mainApp/views.py
+++ django/SakhAparts/mainApp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, HttpResponse
 
 
 def home(request):
-    return render(request, "html/home.html")
+    return render(request, "index.html")
 
 # def list(request):
 #     items = ListItem.objects.all()
